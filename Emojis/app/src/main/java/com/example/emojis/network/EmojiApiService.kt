d5a07461b925a05a9760c18d346177674d77dce2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-23 14:20:37 +0530

    created retrofit builder

--- Emojis/app/src/main/java/com/example/emojis/network/EmojiApiService.kt
+++ Emojis/app/src/main/java/com/example/emojis/network/EmojiApiService.kt
@@ -1,6 +1,25 @@
 package com.example.emojis.network
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 private const val BASE_URL = "https://api.api-ninjas.com/v1/emoji?group="
+
+val interceptor = Interceptor { chain ->
+    val request = chain.request().newBuilder()
+        .addHeader("X-Api-Key", "JX3pyeEMkSPZtsKRfmUAug==4h78z9WI5AvOvotE")
+        .build()
+    chain.proceed(request)
+}
+
+//create retrofit builder
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .client(OkHttpClient().newBuilder().addInterceptor(interceptor).build())
+    .build()
 
 interface EmojiApiService {
 }
