f8aebd163dc38fe53c2885066c0b5f164c1d2874
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-23 15:51:01 +0530

    initialised viewmodel and binding

--- Emojis/app/src/main/java/com/example/emojis/ui/SmileyEmojisFragment.kt
+++ Emojis/app/src/main/java/com/example/emojis/ui/SmileyEmojisFragment.kt
@@ -5,17 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.emojis.R
+import com.example.emojis.databinding.FragmentSmileyEmojisBinding
 
 
 class SmileyEmojisFragment : Fragment() {
+
+    private lateinit var binding: FragmentSmileyEmojisBinding
+    private lateinit var viewModel: EmojisViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_smiley_emojis, container, false)
+        binding = FragmentSmileyEmojisBinding.inflate(inflater,container,false)
+        viewModel = ViewModelProvider(requireActivity()).get(EmojisViewModel::class.java)
+        return binding.root
     }
 
 }
