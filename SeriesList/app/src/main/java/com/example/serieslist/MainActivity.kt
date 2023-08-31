ed80f53862c9825d333012663103a9f09c20b921
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-31 19:36:41 +0530

    created pager

--- SeriesList/app/src/main/java/com/example/serieslist/MainActivity.kt
+++ SeriesList/app/src/main/java/com/example/serieslist/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.serieslist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
+import com.example.serieslist.network.SeriesApi
+import com.example.serieslist.network.SeriesApiService
+import com.example.serieslist.paging.SeriesPagingSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Pager(
+            config = PagingConfig(pageSize = 20, maxSize = 50),
+            pagingSourceFactory = { SeriesPagingSource(seriesApi = SeriesApi.retrofitApiService) }
+        ).liveData.observe(this){
+
+        }
     }
 }
