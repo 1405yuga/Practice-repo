30bfef631b363b19bb6f57784e58aed0dee5edc9
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-31 18:43:03 +0530

    created ApiService interface

--- SeriesList/app/src/main/java/com/example/serieslist/network/SeriesApiService.kt
+++ SeriesList/app/src/main/java/com/example/serieslist/network/SeriesApiService.kt
@@ -1,9 +1,12 @@
 package com.example.serieslist.network
 
+import com.example.serieslist.model.SeriesResult
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-private const val BASE_URL = "https://rickandmortyapi.com/api/character/"
+private const val BASE_URL = "https://rickandmortyapi.com/api/"
 
 //create retrofit builder
 private val retrofitBuilder = Retrofit.Builder()
@@ -12,4 +15,9 @@ private val retrofitBuilder = Retrofit.Builder()
     .build()
 
 interface SeriesApiService {
+
+    //character/?page=2
+    @GET("character/?")
+    suspend fun getCharacters(@Query("page") pageNo: Int): SeriesResult
+
 }
