64536fe3f2d4d27d38b9c6b30a3ec69069fd0ded
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-31 19:15:21 +0530

    load function implemented

--- SeriesList/app/src/main/java/com/example/serieslist/paging/SeriesPagingSource.kt
+++ SeriesList/app/src/main/java/com/example/serieslist/paging/SeriesPagingSource.kt
@@ -6,11 +6,27 @@ import com.example.serieslist.model.Result
 import com.example.serieslist.network.SeriesApiService
 
 class SeriesPagingSource(val seriesApi: SeriesApiService) : PagingSource<Int, Result>() {
+
     override fun getRefreshKey(state: PagingState<Int, Result>): Int? {
-        TODO("Not yet implemented")
+
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult.Page<Int, Result> {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Result> {
+        try {
 
+            //get pageNo
+            val position = params.key ?: 1
+            //load Result
+            val response = seriesApi.getCharacters(position)
+            //create a page
+            return LoadResult.Page(
+                data = response.results,
+                prevKey = if (position == 1) null else position.minus(1),
+                nextKey = if (position == response.info.pages) null else position.plus(1)
+            )
+
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
     }
 }
