b17e2919fb06ce1e2669b77324f8bbae981642f8
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-08-31 20:32:04 +0530

    attached adapter

--- SeriesList/app/src/main/java/com/example/serieslist/MainActivity.kt
+++ SeriesList/app/src/main/java/com/example/serieslist/MainActivity.kt
@@ -2,23 +2,45 @@ package com.example.serieslist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
 import androidx.paging.liveData
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.serieslist.databinding.ActivityMainBinding
 import com.example.serieslist.network.SeriesApi
 import com.example.serieslist.network.SeriesApiService
+import com.example.serieslist.paging.SeriesPagingAdapter
 import com.example.serieslist.paging.SeriesPagingSource
 
+private const val TAG = "MainActivity tag"
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var seriesPagingAdapter: SeriesPagingAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        seriesPagingAdapter = SeriesPagingAdapter()
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = seriesPagingAdapter
+
+        }
 
         Pager(
-            config = PagingConfig(pageSize = 20, maxSize = 50),
+            config = PagingConfig(pageSize = 20, maxSize = 100),
             pagingSourceFactory = { SeriesPagingSource(seriesApi = SeriesApi.retrofitApiService) }
-        ).liveData.observe(this){
-
+        ).liveData.cachedIn(this.lifecycleScope).observe(this){
+            seriesPagingAdapter.submitData(lifecycle,it)
+            Log.d(TAG,"list observer called")
         }
     }
 }
