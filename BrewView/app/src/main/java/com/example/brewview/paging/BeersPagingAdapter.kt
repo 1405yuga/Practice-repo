6540eac3fbceb68b9d9745506522cced3d357c32
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-01 17:06:49 +0530

    created diffUtil object in adapter

--- BrewView/app/src/main/java/com/example/brewview/paging/BeersPagingAdapter.kt
+++ BrewView/app/src/main/java/com/example/brewview/paging/BeersPagingAdapter.kt
@@ -1,13 +1,27 @@
 package com.example.brewview.paging
 
 import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.brewview.databinding.LayoutItemBeerBinding
 import com.example.brewview.model.BeersResult
 import com.example.brewview.model.BeersResultItem
 
-class BeersPagingAdapter : PagingDataAdapter<BeersResultItem,BeersPagingAdapter.BeerViewHolder>() {
+class BeersPagingAdapter : PagingDataAdapter<BeersResultItem,BeersPagingAdapter.BeerViewHolder>(DiffCallBack) {
+    object DiffCallBack : DiffUtil.ItemCallback<BeersResultItem>() {
+        override fun areItemsTheSame(oldItem: BeersResultItem, newItem: BeersResultItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: BeersResultItem,
+            newItem: BeersResultItem
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+    }
 
     class BeerViewHolder(val binding: LayoutItemBeerBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(beer : BeersResultItem){
