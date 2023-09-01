0cda0c5d637e97d188582fa8320785d4b7cf022f
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-01 16:25:31 +0530

    created function to get list

--- BrewView/app/src/main/java/com/example/brewview/network/BeersApiService.kt
+++ BrewView/app/src/main/java/com/example/brewview/network/BeersApiService.kt
@@ -1,9 +1,12 @@
 package com.example.brewview.network
 
+import com.example.brewview.model.BeersResult
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-private const val BASE_URL ="https://api.punkapi.com/v2/"
+private const val BASE_URL = "https://api.punkapi.com/v2/"
 
 //retrofit builder
 private val retrofitBuilder = Retrofit.Builder()
@@ -12,4 +15,12 @@ private val retrofitBuilder = Retrofit.Builder()
     .build()
 
 interface BeersApiService {
+
+    //beers?page=2&per_page=80
+    @GET("beers?")
+    suspend fun getBeersList(
+        @Query("page") pageNo: Int,
+        @Query("per_page") perPage: Int
+    ): BeersResult
+
 }
