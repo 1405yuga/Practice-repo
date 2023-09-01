694a81722066e183e8fc12ea3871535f85164fbb
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-09-01 16:47:07 +0530

    implemented load function in paging source

--- BrewView/app/src/main/java/com/example/brewview/paging/BeersPagingSource.kt
+++ BrewView/app/src/main/java/com/example/brewview/paging/BeersPagingSource.kt
@@ -5,12 +5,22 @@ import androidx.paging.PagingState
 import com.example.brewview.model.BeersResultItem
 import com.example.brewview.network.BeersApiService
 
-class BeersPagingSource(val beersApiService: BeersApiService) : PagingSource<Int,BeersResultItem>() {
+class BeersPagingSource(val beersApiService: BeersApiService) :
+    PagingSource<Int, BeersResultItem>() {
     override fun getRefreshKey(state: PagingState<Int, BeersResultItem>): Int? {
         TODO("Not yet implemented")
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, BeersResultItem> {
-        TODO("Not yet implemented")
+        try {
+            val position: Int = params.key ?: 1
+            val response = beersApiService.getBeersList(position, ProjectConstants.PER_PAGE)
+            return LoadResult.Page(
+                data = response,
+                prevKey = if (position == 1) null else position.minus(1),
+                nextKey = if(position==325) null else position.plus(1))
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
     }
 }
---
+++ BrewView/app/src/main/java/com/example/brewview/paging/ProjectConstants.kt
@@ -0,0 +1,5 @@
+package com.example.brewview.paging
+
+object ProjectConstants{
+    const val PER_PAGE = 10
+}
